aab42297a02628c27b0cc37d6845673b377453af
Sebastian Gradwohl <sebiplay15@gmx.at>
2020-10-22 13:03:04 +0200

    Created some Mock data Integrated into Windows Created Details Window and integrated it into the main window Added Materialsthemes to give it a nice dark theme esk look

---
+++ NtierWH1/Lessons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NtierWH
+{
+    public class Lessons
+    {
+        public string LessonName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Teacher { get; set; }
+    }
+}
---
+++ NtierWH1/ViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NtierWH
+{
+    public class ViewModel
+    {
+        public List<Lessons> LessonList { get; set; }
+
+        private Lessons _SelectedLesson = null;
+        public Lessons SelectedLesson
+        {
+            get { return _SelectedLesson; }
+            set
+            {
+                if (_SelectedLesson != value)
+                {
+                    _SelectedLesson = value;
+                }
+            }
+        }
+
+        public ViewModel()
+        {
+
+            LessonList = new List<Lessons>();
+
+            LessonList.Add(new Lessons()
+            {
+                LessonName = "E",
+                Teacher = "Atzlesberger",
+                Description = "Boring english stuff you'll never need in your entire life"
+            });
+
+            LessonList.Add(new Lessons()
+            {
+                LessonName = "IMC",
+                Teacher = "MSC Christoph Meisinger",
+                Description = "Design and Web Dev"
+            });
+
+            LessonList.Add(new Lessons()
+            {
+                LessonName = "SWPM",
+                Teacher = "DI Markus Meisinger",
+                Description = "Programming stuff and managing Projects"
+            });
+        }
+    }
+}
